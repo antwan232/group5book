29724a96ba193af60ffad6614c7f762f2d37015e
Tony <Tonynabil300@gmail.com>
2025-08-11 14:32:26 +0300

    update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import { useEffect } from "react";
 import PostCard from "./componnents/PostCard/PostCard";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "./store/postSlice";
 import { Toaster } from "react-hot-toast";
 import Footer from "./componnents/Footer/Footer";
-
+import { ProfileProvider, ProfileSidebar } from "./componnents/SlideBar/SlideBar";
 
 
 function App() {
@@ -21,48 +20,43 @@ function App() {
 	const isModalOpened = useSelector((state) => state.posts.modal.isOpen);
 	const modalId = useSelector((state) => state.posts.modal.id);
 
-	
 	return (
-
-		
-
 		<>
-<ProfileProvider >
+			<ProfileProvider>
 				<ProfileSidebar />
-		<div className="bg-gray-900 gap-10 flex flex-col items-center justify-center p-4">
-			{isLoading && <h1>Loading...</h1>}
+				<div className="bg-gray-900 gap-10 flex flex-col items-center justify-center p-4">
+					{isLoading && <h1>Loading...</h1>}
 
-			{posts && (
-				<>
-					<Toaster />
-					{posts?.map((post) => {
-						if (isModalOpened && modalId === post.id) {
-							return (
-								<PostCard
-									key={post.id}
-									post={post}
-								/>
-							);
-						} else if (!isModalOpened) {
-							return (
-								<PostCard
-									key={post.id}
-									post={post}
-								/>
-							);
-						}
-					})}
-				</>
-			)}
+					{posts && (
+						<>
+							<Toaster />
+							{posts?.map((post) => {
+								if (isModalOpened && modalId === post.id) {
+									return (
+										<PostCard
+											key={post.id}
+											post={post}
+										/>
+									);
+								} else if (!isModalOpened) {
+									return (
+										<PostCard
+											key={post.id}
+											post={post}
+										/>
+									);
+								}
+							})}
+						</>
+					)}
 
-			{isError && <h1>Sorry we are not working right now!</h1>}
-		</div>
-	);
+					{isError && <h1>Sorry we are not working right now!</h1>}
+				</div>
+				);
 				<Footer />
-    		</ProfileProvider>
+			</ProfileProvider>
 		</>
-	 );
-
+	);
 }
 
 export default App;
